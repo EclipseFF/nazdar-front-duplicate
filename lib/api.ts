cd34e874f6270631c9f9f9f75b3535feafdbe451
EclipseFF <eclipseff@mail.ru>
2024-08-19 03:31:58 +0500

    dockerfile

--- lib/api.ts
+++ lib/api.ts
@@ -1,3 +1,3 @@
-export const apiUrl = "http://127.0.0.1:4000/api/v1"
+//export const apiUrl = "http://127.0.0.1:4000/api/v1"
 
-//export const apiUrl = "http://91.201.214.206:4001/api/v1"
+export const apiUrl = "http://185.4.180.191:4000/api/v1"
