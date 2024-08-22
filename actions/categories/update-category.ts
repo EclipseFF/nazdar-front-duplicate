c79b38a87e98a1efa922683c87f52f0db1939ddc
EclipseFF <eclipseff@mail.ru>
2024-08-23 01:00:19 +0500

    asd

--- actions/categories/update-category.ts
+++ actions/categories/update-category.ts
@@ -1,7 +1,7 @@
 import {apiUrl} from "@/lib/api";
 
 export default async function UpdateCategory(category: { id: number, name: string }) {
-    const response = await fetch(apiUrl + "/category/" + category.id, {
+    const response = await fetch(apiUrl + "/category", {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
